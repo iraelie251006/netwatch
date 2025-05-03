f886459d233a5902ade9a2620941d29b63a928ba
Elie <niyubwayoiraelie5777@gmail.com>
2025-05-03 21:08:40 +0200

    Fetch & Display Movies

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -2,11 +2,20 @@ import SearchBar from "@/components/SearchBar";
 import { icons } from "@/constants/icons";
 import { images } from "@/constants/images";
 import { useRouter } from "expo-router";
-import { Image, ScrollView, View } from "react-native";
+import { ActivityIndicator, FlatList, Image, ScrollView, Text, View } from "react-native";
 import { StatusBar } from 'expo-status-bar';
+import useFetch from "@/services/useFetch";
+import { fetchMovies } from "@/services/api";
+import MovieCard from "@/components/MovieCard";
 
 export default function Index() {
   const router = useRouter();
+
+  const {
+    data: movies, 
+    loading: moviesLoading, 
+    error: moviesError
+  } = useFetch(() => fetchMovies({query: ""}))
 
   return (
     <View className="flex-1 bg-primary">
@@ -21,12 +30,43 @@ export default function Index() {
         showsVerticalScrollIndicator={false}
       >
         <Image source={icons.logo} className="w-12 h-10 mt-20 mb-5 mx-auto"/>
-        <View className="flex-1 mt-5">
+        {moviesLoading ? (
+          <ActivityIndicator 
+            size="large"
+            color="#0000ff"
+            className="mt-10 self-center"
+          />
+        ) : moviesError ? (
+          <Text>Error: {moviesError?.message}</Text>
+        ): (
+          <View className="flex-1 mt-5">
           <SearchBar 
             onPress={() => router.push("/search")}
             placeholder="Search for a movie"
           />
+
+          <>
+            <Text className="text-white mt-5 text-lg font-bold mb-3">Latest Movies</Text>
+
+            <FlatList 
+              data={movies}
+              renderItem={({item}) => (
+                <MovieCard {...item}/>
+              )}
+              keyExtractor={(item) => item.id}
+              numColumns={3}
+              columnWrapperStyle={{
+                justifyContent: 'flex-start',
+                gap: 20,
+                paddingRight: 5,
+                marginBottom: 10,
+              }}
+              className="mt-2 pb-32"
+              scrollEnabled={false}
+            />
+          </>
         </View>
+        )}
       </ScrollView>
     </View>
   );
--- services/api.ts
+++ services/api.ts
@@ -8,7 +8,7 @@ export const TMDB_CONFIG = {
 }
 
 export const fetchMovies = async({query}: {query: string}) => {
-    const endpoint = query ? `${TMDB_CONFIG.BASE_URL}/discover/movie?query=${encodeURIComponent(query)}` : `${TMDB_CONFIG.BASE_URL}/discover/movie?sort_by=popularity.desc`;
+    const endpoint = query ? `${TMDB_CONFIG.BASE_URL}/search/movie?query=${encodeURIComponent(query)}` : `${TMDB_CONFIG.BASE_URL}/discover/movie?sort_by=popularity.desc`;
 
     const response = await fetch(endpoint, {
         method: 'GET',
@@ -23,18 +23,3 @@ export const fetchMovies = async({query}: {query: string}) => {
     const data = await response.json();
     return data.results;
 }
-
-
-// const url = 'https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc';
-// const options = {
-//   method: 'GET',
-//   headers: {
-//     accept: 'application/json',
-//     Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzN2FiZTY2YzM1ODUxNGE5NmUwOWU5NzJlYWViYzYwMiIsIm5iZiI6MS43NDYxOTcwNTY5OTEwMDAyZSs5LCJzdWIiOiI2ODE0ZGE0MGVmZWQ4N2M3ZGY5NjgzYzQiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0._Ou5y-VRmcIRnWbwkTfF7kuCt79xfx14aqLX7Q0fsZ4'
-//   }
-// };
-
-// fetch(url, options)
-//   .then(res => res.json())
-//   .then(json => console.log(json))
-//   .catch(err => console.error(err));

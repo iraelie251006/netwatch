7b6ae624dacaf3c5866c69f2f30b2568fcc1db1a
Elie <niyubwayoiraelie5777@gmail.com>
2025-05-03 23:43:39 +0200

    Fixing SearchBar making some props optional

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -4,8 +4,8 @@ import { View, Image, TextInput } from 'react-native'
 interface Props {
     placeholder: string;
     onPress?: () => void;
-    value: string;
-    onChangeText: (text: string) => void;
+    value?: string;
+    onChangeText?: (text: string) => void;
 }
 const SearchBar = ({placeholder, onPress, value, onChangeText}: Props) => {
   return (

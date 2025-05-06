1b45ef5d22a3f2084a5e5212b57987db9602e97f
Elie <niyubwayoiraelie5777@gmail.com>
2025-05-06 23:12:13 +0200

    Showing Trending Movies part 1

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -7,10 +7,16 @@ import { StatusBar } from 'expo-status-bar';
 import useFetch from "@/services/useFetch";
 import { fetchMovies } from "@/services/api";
 import MovieCard from "@/components/MovieCard";
+import { getTrendingMovies } from "@/services/appwrite";
 
 export default function Index() {
   const router = useRouter();
 
+  const {
+    data: trendingMovies, 
+    loading: trendingLoading, 
+    error: trendingError
+  } = useFetch(getTrendingMovies)
   const {
     data: movies, 
     loading: moviesLoading, 
@@ -30,20 +36,32 @@ export default function Index() {
         showsVerticalScrollIndicator={false}
       >
         <Image source={icons.logo} className="w-12 h-10 mt-20 mb-5 mx-auto"/>
-        {moviesLoading ? (
+        {moviesLoading || trendingLoading ? (
           <ActivityIndicator 
             size="large"
             color="#0000ff"
             className="mt-10 self-center"
           />
-        ) : moviesError ? (
-          <Text>Error: {moviesError?.message}</Text>
+        ) : moviesError || trendingError ? (
+          <Text>Error: {moviesError?.message || trendingError?.message}</Text>
         ): (
           <View className="flex-1 mt-5">
           <SearchBar 
             onPress={() => router.push("/search")}
             placeholder="Search for a movie"
           />
+
+          {trendingMovies && (
+            <View className="mt-10">
+              <Text className="text-lg text-white font-bold mb-3">Trending Movies</Text>
+
+              <FlatList 
+                data={trendingMovies}
+                renderItem={({item, index}) => ()}
+                className="mb-4 mt-3"
+              />
+            </View>
+          )}
 
           <>
             <Text className="text-white mt-5 text-lg font-bold mb-3">Latest Movies</Text>
